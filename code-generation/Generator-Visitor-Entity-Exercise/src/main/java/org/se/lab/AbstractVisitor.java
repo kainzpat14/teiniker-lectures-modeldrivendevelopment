1ed31fb47006a38508811c3df0bb79bd75452795
Patrick Kainz-Lieb <patrick.kainz2@fh-joanneum.at>
2024-01-09 18:07:33 +0100

    removed bean stuff way to much work

--- code-generation/Generator-Visitor-Entity-Exercise/src/main/java/org/se/lab/AbstractVisitor.java
+++ code-generation/Generator-Visitor-Entity-Exercise/src/main/java/org/se/lab/AbstractVisitor.java
@@ -1,5 +1,10 @@
 package org.se.lab;
 
+import org.se.lab.metamodel.MInteger;
+import org.se.lab.metamodel.MString;
+import org.se.lab.metamodel.MType;
+
 public abstract class AbstractVisitor implements Visitor {
+    @Override
 
 }
