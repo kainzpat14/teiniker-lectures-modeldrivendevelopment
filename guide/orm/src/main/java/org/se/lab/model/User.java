65af36104fde8542c0fe9b7642dedf7cd30e5064
Patrick Kainz-Lieb <patrick.kainz2@fh-joanneum.at>
2025-02-09 10:50:37 +0100

    Added JPA example

---
+++ guide/orm/src/main/java/org/se/lab/model/User.java
@@ -0,0 +1,67 @@
+package org.se.lab.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Objects;
+
+@Entity
+public class User {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
